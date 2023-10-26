7a64efd62db66c3c1cb65b87162cd8ec1bb49e85
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-26 11:51:21 -0300

    communicated user controllers

--- gestion/src/main/java/com/egestion/gestion/controllers/UsuarioController.java
+++ gestion/src/main/java/com/egestion/gestion/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.egestion.gestion.controllers;
 
+import com.egestion.gestion.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,46 @@ import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario obtenerUsuario(@PathVariable String id){
+        Usuario usuario = new Usuario();
+        usuario.setId(Long.valueOf("123456"));
+        usuario.setEmail("denisroldan@mail.com");
+        usuario.setApellido("roldan");
+        usuario.setNombre("denis");
+        usuario.setPassword("123456");
+        usuario.setTelefono("321321321");
+        return usuario;
+    }
+    @RequestMapping(value = "usuarios1")
+    public Usuario editarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("denisroldan@mail.com");
+        usuario.setApellido("roldan");
+        usuario.setNombre("denis");
+        usuario.setPassword("123456");
+        usuario.setTelefono("321321321");
+        return usuario;
+    }
+    @RequestMapping(value = "usuarios2")
+    public Usuario eliminarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("denisroldan@mail.com");
+        usuario.setApellido("roldan");
+        usuario.setNombre("denis");
+        usuario.setPassword("123456");
+        usuario.setTelefono("321321321");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuarios3")
+    public Usuario buscarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setEmail("denisroldan@mail.com");
+        usuario.setApellido("roldan");
+        usuario.setNombre("denis");
+        usuario.setPassword("123456");
+        usuario.setTelefono("321321321");
+        return usuario;
     }
 }
