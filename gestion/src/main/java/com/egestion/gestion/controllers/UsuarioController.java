09281aa84ab5df53bbc8c7c28564f160a509d7e9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-26 11:18:57 -0300

    User model

--- gestion/src/main/java/com/egestion/gestion/controllers/UsuarioController.java
+++ gestion/src/main/java/com/egestion/gestion/controllers/UsuarioController.java
@@ -3,10 +3,12 @@ package com.egestion.gestion.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
     @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    public List<String> prueba(){
+        return List.of("prueba","prueba2","prueba3");
     }
 }
---
+++ gestion/src/main/java/com/egestion/gestion/models/Usuario.java
@@ -0,0 +1,10 @@
+package com.egestion.gestion.models;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private String telefono;
+
+}
